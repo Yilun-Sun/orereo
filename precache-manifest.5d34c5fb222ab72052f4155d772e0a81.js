ce6753efb839d062f61337d71d5613a6324515f1
Yilun-Sun <osirisnero@gmail.com>
2020-02-11 23:59:04 -0500

    Updates

---
+++ precache-manifest.5d34c5fb222ab72052f4155d772e0a81.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b259084423b071daab00cbf1273a77",
+    "url": "/orereo/index.html"
+  },
+  {
+    "revision": "358ea44081cdcdffc7db",
+    "url": "/orereo/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "298381320fd2b6d03f34",
+    "url": "/orereo/static/css/main.a2308890.chunk.css"
+  },
+  {
+    "revision": "358ea44081cdcdffc7db",
+    "url": "/orereo/static/js/2.13794081.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/orereo/static/js/2.13794081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298381320fd2b6d03f34",
+    "url": "/orereo/static/js/main.66bd6877.chunk.js"
+  },
+  {
+    "revision": "13ba17136d6965774024",
+    "url": "/orereo/static/js/runtime-main.f3998cdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/orereo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed2be46e8af3e665b74b6b77268f68a4",
+    "url": "/orereo/static/media/o.ed2be46e.png"
+  },
+  {
+    "revision": "7d5b065174a2bc99826a6c6e97f37183",
+    "url": "/orereo/static/media/re.7d5b0651.png"
+  }
+]);
